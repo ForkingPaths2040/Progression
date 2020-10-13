cb9d3a054c24447f60f65a62f9b4c9cd3e256905
Otto Rotor <ambermoorexc@gmail.com>
2020-10-13 13:22:35 -0500

    Last commit that's written badly

--- src/Entries.jsx
+++ src/Entries.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
-function Entries () {
+function Entries() {
+  const [entries, setEntries] = useState([]);
+  useEffect(() => {
+    const getEntries = async () => {
+      const airtableURL = `https://api.airtable.com/v0/appsnyAAoewIo80Ig/progressions`;
+      const response = await axios.get(airtableURL, {
+        headers: {
+          Authorization: `Bearer keyA4pUKd1DMLTmI9`,
+        },
+      });
+      setEntries(response.data.records);
+    };
+    getEntries();
+  }, []);
     return (
       <div>
-        <img src="https://media.giphy.com/media/CeCQXGeb6Z3V6uDN3D/giphy.gif" alt="ollie" />
+        {entries.map((entry) => (
+          <div className='entry-container' key={entry.id}>
+            <h4 className='entry-cells'>{entry.fields.trick}</h4>
+            <h4 className='entry-cells'>{entry.fields.username}</h4>
+            <h4 className='entry-cells'>{entry.fields.stance}</h4>
+            <h4 className='entry-cells'>{entry.fields.type}</h4>
+            <h4 className='entry-cells'>{entry.fields.landing}</h4>
+            </div>
+        ))}
       </div>
 
     );
--- src/TrickPage.jsx
+++ src/TrickPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams, Link, Route } from 'react-router-dom'
+import { useParams, Link} from 'react-router-dom'
 
 
 function TrickPage(props) {
