599306f5eb5458b60e06d99613b0360a5a929811
Otto Rotor <ambermoorexc@gmail.com>
2020-10-14 09:57:44 -0500

    New Components added to reach MVP

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer(props) {
+  return (
+    <div>
+      Data and Images provided by: 
+      <a href="https://www.skateboardhere.com/skateboard-trick-list.html">this</a>
+      <a href="https://giphy.com/">giphy</a>
+
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/Components/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomePage(props) {
+  return (
+    <div className='home-page'>
+      <h1>What will you improve today?</h1>
+    </div>
+  );
+}
+
+export default HomePage;
