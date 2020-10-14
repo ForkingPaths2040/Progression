6a36d7dcaee26d5c92f6e7c082f86af1796151c1
Otto Rotor <ambermoorexc@gmail.com>
2020-10-14 13:48:44 -0500

    Layout tricks in a grid with added class names to reflect changes

--- src/Components/ListTricks.jsx
+++ src/Components/ListTricks.jsx
@@ -4,16 +4,18 @@ import { Link } from 'react-router-dom'
 function ListTricks(props) {
 
   return (
+    <div className="page-length">
     <div className= "all-tricks">
       {props.tricks.map((trick) => (
         <div className="trick-container" key={trick.id}>
-          <Link to={`/tricks/${trick.fields.name}`}>
-            <button className="list-button">{trick.fields.name}</button>
+          <Link to={`/tricks/${trick.fields.name}`} style={{textDecoration: 'none'}}>
+            <div className="list-button">{trick.fields.name}</div>
             </Link>
         </div>
       ))}
       
-    </div>
+      </div>
+      </div>
   );
 }
 
